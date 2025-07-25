0a1cda578ef24734c93f4151321e0857e5f220ca
juniorpagedown <81428565+juniorpagedown@users.noreply.github.com>
2025-07-25 06:24:25 -0300

    fix: corrigir configuração do Prisma e Next.js para deploy no Vercel

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CjGTE5fYQHwP7vf0cRdhv16j/jHLzs+a2Q+etiM4lOQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XrKpqjaNr4nIOeQOeZmJ8U8i2JfzAwja+xpAp0WwpJU=\"\n}"
